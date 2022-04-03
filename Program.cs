b4d06b5a82804bfc1eef45ffa737731fa3aa6051
Thorsten Fieger <Thorsten.Fieger@gmail.com>
2022-04-03 12:31:48 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Lumia;
+
+
+namespace LumiaControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string token = new string("lumia892089382");
+            string IP = new string("192.168.0.39");
+
+            LumiaSdk framework = new LumiaSdk();
+
+
+
+
+        }
+    }
+}
